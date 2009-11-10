e76f2d320b2103c698d55193f8c61addcada5b25
casey provost <caseyprovost@casey-provosts-macbook-pro-1123.local>
2009-11-10 10:34:00 -0500

    just for fun commit

---
+++ system/logs/log-2009-11-10.php
@@ -0,0 +1,4 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2009-11-10 10:33:06 --> 404 Page Not Found --> admin/posts
+ERROR - 2009-11-10 10:33:06 --> 404 Page Not Found --> admin/posts
